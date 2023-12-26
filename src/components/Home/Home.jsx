cbd2a2a9a9ceb392b209a283abda07f693b9c3dd
Shariful4447 <shariful.islam4447@gmail.com>
2023-12-26 07:29:06 +0100

    footer component placement changed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../NavBar/Navbar";
-import Footer from "../../Footer/Footer";
+import Footer from "../Footer/Footer";
 
 
 const Home = () => {
